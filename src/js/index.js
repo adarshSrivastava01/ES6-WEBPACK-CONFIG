9b8eae34541e707b851589647a649c139aff26d7
adarshSrivastava01 <herecomesthebeastlesnar@gmail.com>
2020-05-06 22:11:35 +0530

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import str from './models/Model';
+import { sum1, min1} from './views/view';
+
+console.log(`Hey! ${str}`);
+console.log(sum1(15,5));
+console.log(min1(15,2));
---
+++ src/js/views/view.js
@@ -0,0 +1,2 @@
+export const sum1 = (a,b) => a + b;
+export const min1 = (a,b) => a - b;
